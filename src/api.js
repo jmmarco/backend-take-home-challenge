51cef2438f4b6906937dc7fdcf8661becdd9b70f
Juan Marco <jmmarco@gmail.com>
2024-06-08 17:46:09 +0200

    Update getDailyWeather function to handle missing API_KEY

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
-const getDailyWeather = (city = "") => {
+const getDailyWeather = (city = '') => {
+  if (!process.env.API_KEY) {
+    throw new Error('API_KEY is not set');
+  }
   const endpoint = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=${process.env.API_KEY}`;
   return fetch(endpoint)
     .then((response) => {
